32ba16055d59e088f5dab49ba46ddab7e8f51b1e
Silviu Simen <silviu.simen@gmail.com>
2019-02-13 20:30:46 -0500

    PDF reduce script

---
+++ pdf-reduce.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+
+FILE="$1"
+MODE="$2"
+
+if [ -z "${MODE}" ]; then 
+    echo "Prerequisite: sudo apt install ghostscript"
+    echo "Usage $0 filane.pdf mode"
+    echo "mode can be one of screen, ebook, printer, prepress"
+else
+    ps2pdf -dPDFSETTINGS=/${MODE} ${FILE} ${FILE%%.pdf}_${MODE}.pdf
+fi
